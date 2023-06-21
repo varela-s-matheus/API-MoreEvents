1caaf4cf334f1bcd27c67b627ecdc4e2a4708e18
Matheus Varela <matheus_s_varela@hotmail.com>
2023-06-20 23:58:04 -0300

    Fix to deploy API

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 
-const port = process.env.PORT || 3000;
+const port = "https://api-more-events.vercel.app";
 
 app.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
+    console.log(`Servidor escutando em ${port}`);
 })
